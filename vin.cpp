7dafa54d49bc1ed72d9bc864a3ef1b07ed1a94c5
vensan <vensan@me.com>
2022-09-26 22:31:00 +1300

    Add util.

--- vin.cpp
+++ vin.cpp
@@ -11,21 +11,7 @@
 #include <string>
 #include <vulkan/vulkan.hpp>
 
-void verify(bool expr) {
-#ifndef NDEBUG
-    if (!(expr)) {
-        abort();
-    }
-#endif
-}
-
-void error(std::string_view err_msg, std::string_view err_class) {
-    do {
-        MessageBox(nullptr, err_msg.data(), err_class.data(), MB_OK);
-        exit(1);
-    } while (0);
-}
-
+#include "util.h"
 #include "linmath.h" // [TODO] Use propre classes instead of arrays.
 #include "font.h"
 #include "text.h"
