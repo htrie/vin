f7727821599c67e098a8b62bea3de0d0d08568b1
vensan <vensan@me.com>
2022-10-20 23:11:52 +1300

    E-ink colors.

--- config.h
+++ config.h
@@ -1,23 +1,23 @@
 #pragma once
 
 struct Colors {
-	Color clear = Color::rgba(1, 22, 39, 255);
-	Color overlay = Color::rgba(31, 52, 69, 255);
-	Color cursor = Color::rgba(255, 255, 0, 255);
-	Color cursor_line = Color::rgba(65, 80, 29, 255);
-	Color whitespace = Color::rgba(15, 40, 33, 255);
-	Color text = Color::rgba(205, 226, 239, 255);
-	Color text_cursor = Color::rgba(5, 5, 5, 255);
-	Color line_number = Color::rgba(75, 100, 121, 255);
-	Color highlight = Color::rgba(255, 0, 255, 255);
-	Color diff_note = Color::rgba(255, 192, 0, 255);
-	Color diff_add = Color::rgba(0, 192, 0, 255);
-	Color diff_remove = Color::rgba(192, 0, 0, 255);
-	Color cpp_comment = Color::rgba(255, 255, 0, 255);
-	Color cpp_keyword = Color::rgba(199, 146, 234, 255);
-	Color cpp_punctuation = Color::rgba(127, 219, 202, 255);
-	Color cpp_number = Color::rgba(247, 140, 108, 255);
-	Color cpp_string = Color::rgba(247, 140, 108, 255);
+	Color clear = Color::rgba(247, 247, 247, 255);
+	Color overlay = Color::rgba(227, 227, 227, 255);
+	Color cursor = Color::rgba(0, 0, 0, 255);
+	Color cursor_line = Color::rgba(196, 196, 196, 255);
+	Color whitespace = Color::rgba(220, 220, 220, 255);
+	Color text = Color::rgba(50, 50, 50, 255);
+	Color text_cursor = Color::rgba(220, 220, 220, 255);
+	Color line_number = Color::rgba(180, 180, 180, 255);
+	Color highlight = Color::rgba(148, 148, 148, 255);
+	Color diff_note = Color::rgba(128, 128, 128, 255);
+	Color diff_add = Color::rgba(0, 0, 0, 255);
+	Color diff_remove = Color::rgba(200, 200, 200, 255);
+	Color cpp_comment = Color::rgba(200, 200, 200, 255);
+	Color cpp_keyword = Color::rgba(0, 0, 0, 255);
+	Color cpp_punctuation = Color::rgba(128, 128, 128, 255);
+	Color cpp_number = Color::rgba(100, 100, 100, 255);
+	Color cpp_string = Color::rgba(80, 80, 80, 255);
 };
 
 const Colors colors() {
