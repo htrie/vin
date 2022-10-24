4cdb0c14c8b5c92cb7025d17a084ff7d1cc968ae
vensan <vensan@me.com>
2022-10-25 00:18:27 +1300

    Put back colors.

--- config.h
+++ config.h
@@ -1,22 +1,22 @@
 #pragma once
 
 struct Colors {
-	Color clear = Color::gray(247);
-	Color overlay = Color::gray(237);
-	Color cursor = Color::gray(0);
-	Color cursor_line = Color::gray(217);
-	Color whitespace = Color::gray(240);
-	Color text = Color::gray(5);
-	Color text_cursor = Color::gray(220);
-	Color line_number = Color::gray(140);
-	Color highlight = Color::gray(188);
-	Color diff_note = Color::gray(128);
-	Color diff_add = Color::gray(0);
-	Color diff_remove = Color::gray(166);
-	Color cpp_comment = Color::gray(166);
-	Color cpp_punctuation = Color::gray(158);
-	Color cpp_quote = Color::gray(178);
-	Color cpp_string = Color::gray(80);
+	Color clear = Color::rgba(1, 22, 39, 255);
+	Color overlay = Color::rgba(31, 52, 69, 255);
+	Color cursor = Color::rgba(255, 255, 0, 255);
+	Color cursor_line = Color::rgba(65, 80, 29, 255);
+	Color whitespace = Color::rgba(15, 40, 33, 255);
+	Color text = Color::rgba(205, 226, 239, 255);
+	Color text_cursor = Color::rgba(5, 5, 5, 255);
+	Color line_number = Color::rgba(75, 100, 121, 255);
+	Color highlight = Color::rgba(255, 0, 255, 255);
+	Color diff_note = Color::rgba(255, 192, 0, 255);
+	Color diff_add = Color::rgba(0, 192, 0, 255);
+	Color diff_remove = Color::rgba(192, 0, 0, 255);
+	Color cpp_comment = Color::rgba(255, 255, 0, 255);
+	Color cpp_punctuation = Color::rgba(127, 219, 202, 255);
+	Color cpp_quote = Color::rgba(247, 140, 108, 255);
+	Color cpp_string = Color::rgba(247, 140, 108, 255);
 };
 
 const Colors colors() {
