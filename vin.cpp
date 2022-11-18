3c9f3e721ce2a76bb2de0b210cb81852e00f7720
vincent <vensan@me.com>
2022-11-18 18:56:06 +1300

    Bump version.

--- vin.cpp
+++ vin.cpp
@@ -65,7 +65,7 @@ class App {
 	void set_dirty(bool b) { dirty = b; }
 
 	std::string status() {
-		return std::string("Vin v0.4  ") +
+		return std::string("Vin v0.5  ") +
 			std::to_string(switcher.current().location_percentage()) + "%  " +
 			std::string(switcher.current().get_filename()) + 
 			(switcher.current().is_dirty() ? "*" : "") + "  " +
