1d4563c2b3fafed0380961cc0906f7072bd430ff
vincent <vensan@me.com>
2022-11-20 16:16:04 +1300

    todo

--- index.h
+++ index.h
@@ -53,11 +53,6 @@ class Database {
 
 public:
 	void populate() {
-		// go through all files
-		// open and parse all words
-		// store all words filenames and locations
-		// avoid storing filename multiple times
-		// use unordered map
 	}
 };
 
