283760720bd1a2c34f57e3525cd5d652bd22444f
vincent <vensan@me.com>
2022-12-18 13:56:29 +1300

    Remove test.

--- test/Resource/FontResource.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef USE_FREETYPE
-#	include "FontResourceFreeType.h"
-typedef FontResourceFreeType FontResource;
-#else
-#	include "FontResourceWindows.h"
-typedef FontResourceWindows FontResource;
-#endif
