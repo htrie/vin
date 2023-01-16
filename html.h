2705a6edd659f361c62847739220e74a7f5f9249
vincent <vensan@me.com>
2023-01-16 23:03:31 +1300

    Parse unicode.

--- html.h
+++ html.h
@@ -11,6 +11,7 @@ class Html {
 		address_footer,
 		paragraph_header,
 		paragraph_body,
+		paragraph_body_unicode,
 		paragraph_footer,
 		paragraph_nested_header,
 		paragraph_nested_body,
@@ -88,6 +89,15 @@ class Html {
 	void process_paragraph_footer(const HugeString& text, size_t pos) {
 		if (text[pos] == '>') { tag = Tag::none; }
 	}
+	void process_paragraph_body_unicode(const HugeString& text, size_t pos) {
+		if (text[pos] == -128) { /* Skip */ }
+		else if (text[pos] == -90) { append_paragraph('.'); append_paragraph('.'); append_paragraph('.'); tag = Tag::paragraph_body; }
+		else if (text[pos] == -99) { append_paragraph('"'); tag = Tag::paragraph_body; }
+		else if (text[pos] == -100) { append_paragraph('"'); tag = Tag::paragraph_body; }
+		else if (text[pos] == -103) { append_paragraph('\''); tag = Tag::paragraph_body; }
+		else if (text[pos] == -108) { append_paragraph('-'); tag = Tag::paragraph_body; }
+		else { append_paragraph(text[pos]); tag = Tag::paragraph_body; }
+	}
 	void process_paragraph_body(const HugeString& text, size_t pos) {
 		if (text.substr(pos, 4) == SmallString("<em>")) { tag = Tag::paragraph_nested_header; }
 		else if (text.substr(pos, 6) == SmallString("<code>")) { tag = Tag::paragraph_nested_header; }
@@ -95,6 +105,7 @@ class Html {
 		else if (text.substr(pos, 3) == SmallString("<i>")) { tag = Tag::paragraph_nested_header; }
 		else if (text.substr(pos, 4) == SmallString("<a >")) { tag = Tag::paragraph_nested_header; }
 		else if (text[pos] == '<') { print_paragraph(); tag = Tag::paragraph_footer; }
+		else if (text[pos] == -30) { tag = Tag::paragraph_body_unicode; }
 		else if (text[pos] == '\n') { append_paragraph(' '); }
 		else if (text[pos] == 13/*CR*/) { append_paragraph(' '); }
 		else if (text[pos] == '\t') { append_paragraph(' '); }
@@ -130,6 +141,7 @@ class Html {
 			case Tag::address_footer: process_address_footer(text, pos); break;
 			case Tag::paragraph_header: process_paragraph_header(text, pos); break;
 			case Tag::paragraph_body: process_paragraph_body(text, pos); break;
+			case Tag::paragraph_body_unicode: process_paragraph_body_unicode(text, pos); break;
 			case Tag::paragraph_footer: process_paragraph_footer(text, pos); break;
 			case Tag::paragraph_nested_header: process_paragraph_nested_header(text, pos); break;
 			case Tag::paragraph_nested_body: process_paragraph_nested_body(text, pos); break;
