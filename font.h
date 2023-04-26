03c1eb358f45a80e6ace0bbf64e6e52148d56c4d
vensan <vensan@me.com>
2023-04-26 20:51:18 +1200

    Font fallback.

--- font.h
+++ font.h
@@ -330,8 +330,20 @@ struct Font {
 		return 0;
 	}
 
+	std::string check_font(const std::string& font) {
+		if (std::filesystem::exists(font))
+			return font;
+		return {};
+	}
+
+	std::string find_font() {
+		if (const auto font = check_font(get_user_font_path() + "PragmataPro_Mono_R_liga.ttf"); !font.empty())
+			return font;
+		return check_font(get_system_font_path() + get_system_font_name("Consolas"));
+	}
+
 	Font()
-		: file(get_user_font_path() + "PragmataPro_Mono_R_liga.ttf") { //(get_system_font_path() + get_system_font_name("Consolas")) // TODO fallback
+		: file(find_font()) {
 		if (!is_safe_offset(0, 12))
 			return;
 		/* Check for a compatible scalerType (magic number). */
